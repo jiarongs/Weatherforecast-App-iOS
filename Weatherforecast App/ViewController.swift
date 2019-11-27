23c2d67a977dd4246c544c8e640c658be85ba077
jiarongs <jiarongs@usc.edu>
2019-11-26 18:11:05 -0800

    Initial Commit

---
+++ Weatherforecast App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weatherforecast App
+//
+//  Created by 司佳蓉 on 11/26/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Weatherforecast AppTests/Weatherforecast_AppTests.swift
@@ -0,0 +1,33 @@
+//
+//  Weatherforecast_AppTests.swift
+//  Weatherforecast AppTests
+//
+//  Created by 司佳蓉 on 11/26/19.
+//
+
+import XCTest
+@testable import Weatherforecast_App
+
+class Weatherforecast_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
